c0a921d08f243d3d814913acae2967038d26cabc
Thato Tshabalala <65793734+danya247@users.noreply.github.com>
2025-06-04 11:22:50 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TTDevsLanding from './pages/index.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TTDevsLanding />
+  </React.StrictMode>
+);
